7400a278dffac1ccce65401efc12c44561dd2b8b
intelaashish <aashish73134@gmail.com>
2023-05-19 15:07:34 -0700

    add method to calculate accuracy.

--- source/helper_functions.py
+++ source/helper_functions.py
@@ -296,3 +296,8 @@ def download_data(source: str,
             os.remove(data_path / target_file)
     
     return image_path
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100 
+    return acc
