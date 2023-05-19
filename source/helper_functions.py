e92819cfa8dc456ea958d5a8512af8c5293b3dfc
intelaashish <aashish73134@gmail.com>
2023-05-18 21:01:11 -0700

    change color schemes.

--- source/helper_functions.py
+++ source/helper_functions.py
@@ -44,7 +44,7 @@ def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Ten
     # Put everything to CPU (works better with NumPy + Matplotlib)
     model.to("cpu")
     X, y = X.to("cpu"), y.to("cpu")
-
+    
     # Setup prediction boundaries and grid
     x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
     y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
@@ -52,24 +52,27 @@ def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Ten
 
     # Make features
     X_to_pred_on = torch.from_numpy(np.column_stack((xx.ravel(), yy.ravel()))).float()
-
+    
+    
     # Make predictions
     model.eval()
     with torch.inference_mode():
         y_logits = model(X_to_pred_on)
-
+    
     # Test for multi-class or binary and adjust logits to prediction labels
     if len(torch.unique(y)) > 2:
         y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)  # mutli-class
     else:
         y_pred = torch.round(torch.sigmoid(y_logits))  # binary
-
+    
     # Reshape preds and plot
     y_pred = y_pred.reshape(xx.shape).detach().numpy()
-    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    # plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7) 
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.rainbow, alpha=0.7) 
     plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
+    
 
 
 # Plot linear data or training and test and predictions (optional)
@@ -82,10 +85,10 @@ def plot_predictions(
     plt.figure(figsize=(10, 7))
 
     # Plot training data in blue
-    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
+    plt.scatter(train_data, train_labels, c="g", s=4, label="Training data")
 
     # Plot test data in green
-    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+    plt.scatter(test_data, test_labels, c="r", s=4, label="Testing data")
 
     if predictions is not None:
         # Plot the predictions in red (predictions were made on the test data)
